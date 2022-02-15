bbcf56034c58ac294a266777078fa7f70c859296
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-15 08:50:59 +0100

    product

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./product.css";
-
-function Product() {
+import StarIcon from "@mui/icons-material/Star";
+function Product({ name, stars, price }) {
   return (
     <div className="product">
-      <h3>Title Product</h3>
-      <p>$339.99</p>
-      <div>Stars</div>
+      <h3>{name && name}</h3>
+      <p>{price && price}</p>
+      <div className="stars">
+        {stars &&
+          [...Array(stars)].map((e) => {
+            return <StarIcon />;
+          })}
+      </div>
       <img src="logo192.png" />
       <button>Add to Cart</button>
     </div>
