c0d8cbf34e8813dbda2505674f56cb55688ab0eb
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-02-15 09:52:42 +0100

    commit

--- src/components/checkout/Checkout.js
+++ src/components/checkout/Checkout.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../../userContext";
 import "./checkout.css";
 
 function Checkout() {
+  const counts = {};
+  const { cart, setCart } = useContext(UserContext);
+  cart.forEach((x) => {
+    counts[x.id] = (counts[x.id] || 0) + 1;
+  });
+  console.log(counts);
+
+  cart.filter((cart) => {
+    for (let i = 0; i < cart.length; i++) {}
+  });
+
   return (
     <div className="checkout">
       <div>Products</div>
